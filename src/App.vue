3c5c1984085b3900b4c2b2ae28f39bac005db55a
Joan Morell <noogic@gmail.com>
2022-11-28 19:40:39 +0100

    Calls can be removed and added

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@
                 <div class="mt-12 flex justify-between">
                     <div class="flex text-slate-700">
                         <div>
-                            Current cost: <span class="ml-3 font-bold text-lg text-amber-500">{{ Number(costs).toFixed(5) }}</span>
+                            Current cost: <span class="ml-1 font-bold text-lg text-amber-500">{{ Number(costs).toFixed(5) }}</span>
                         </div>
-                        <div class="ml-5">
+                        <div class="ml-6">
                             <span class="text-slate-500">Databases</span>
                             <input
                                 type="number"
@@ -30,37 +30,68 @@
             </div>
             <div class="mt-12">
                 <h2 class="text-xl font-bold text-slate-700">Calls</h2>
-                <div class="mt-6 space-y-4">
-                    <div class="px-4 pb-1 grid grid-cols-4 border-b border-slate-300">
+                <div class="mt-6">
+                    <div class="px-4 pb-1 grid grid-cols-8 border-b border-slate-300">
                         <div class="col-span-2">Type</div>
                         <div>Lines</div>
                     </div>
-                    <div v-for="(call, index) in calls" :key="call.type.type" class="shadow">
-                        <div class="p-4 grid grid-cols-4 bg-white rounded-lg">
-                            <div class="col-span-2">
-                                <div class="font-semibold">
-                                    {{ call.type.name }}
+                    <div class="mt-4 space-y-4">
+                        <div v-for="(call, index) in calls" :key="call.type.type" class="shadow">
+                            <div class="p-4 grid grid-cols-8 bg-white rounded-lg">
+                                <div class="col-span-6">
+                                    <div class="font-semibold">
+                                        {{ call.type.name }}
+                                    </div>
+                                    <div class="mt-1 text-sm text-slate-500">
+                                        {{ call.type.description }}
+                                    </div>
+                                    <a :href="call.type.link" class="text-xs text-blue-300" target="_blank">
+                                        {{ call.type.link }}
+                                    </a>
                                 </div>
-                                <div class="mt-1 text-sm text-slate-500">
-                                    {{ call.type.description }}
+                                <div>
+                                    <input
+                                        type="number"
+                                        v-model="call.quantity"
+                                        class="px-2 border-0 border-b border-b-amber-500 text-slate-700 font-semibold focus:border-indigo-600 focus:ring-0"
+                                        :max="call.type.max_database_results"
+                                    />
                                 </div>
-                                <a :href="call.type.link" class="text-xs text-blue-300">
-                                    {{ call.type.link }}
-                                </a>
+                                <div class="flex justify-end items-center">
+                                    <button
+                                        class="hover:text-amber-600 hover:font-black"
+                                        @click="calls = calls.filter(item => item !== call)"
+                                    >
+                                        <XMarkIcon class="h-5 w-5"/>
+                                    </button>
+                                </div>
                             </div>
-                            <div>
-                                <input
-                                    type="number"
-                                    v-model="call.quantity"
-                                    class="px-2 border-0 border-b border-b-amber-500 text-slate-700 font-semibold focus:border-indigo-600 focus:ring-0"
-                                    :max="call.type.max_database_results"
-                                />
-                            </div>
-                            <div class="flex justify-end items-center">
-                                <XMarkIcon
-                                    class="h-5 w-5 cursor-pointer hover:text-amber-600 hover:font-black"
-                                    @click="calls = calls.filter(item => item !== call)"
-                                />
+                        </div>
+                    </div>
+
+                    <div class="mt-16" v-if="unusedCalls.length > 0">
+                        <h2 class="text-xl font-bold text-slate-700">Unused Calls</h2>
+                        <div v-for="(call, index) in unusedCalls" :key="call.name" class="mt-4 shadow">
+                            <div class="p-4 grid grid-cols-8 bg-white rounded-lg">
+                                <div class="col-span-7">
+                                    <div class="font-semibold">
+                                        {{ call[1].name }}
+                                    </div>
+                                    <div class="mt-1 text-sm text-slate-500">
+                                        {{ call[1].description }}
+                                    </div>
+                                    <a :href="call[1].link" class="text-xs text-blue-300" target="_blank">
+                                        {{ call[1].link }}
+                                    </a>
+                                </div>
+                                <div class="flex justify-end items-center">
+                                    <button
+                                        class="hover:text-amber-600 hover:font-black"
+                                        @click="addCall(call[1].type)"
+                                    >
+                                        <PlusIcon class="h-5 w-5"/>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -72,7 +103,7 @@
 
 <script setup>
 import { computed, ref } from "vue";
-import { XMarkIcon } from '@heroicons/vue/24/outline';
+import { XMarkIcon, PlusIcon } from '@heroicons/vue/24/outline';
 
 const types = {
     domain_ranks: {
@@ -127,4 +158,13 @@ const costs = computed(() => calls.value.reduce(
     0
 ) * unitPrice.value * databases.value);
 
+const unusedCalls = computed(() => Object.entries(types).filter(type => !calls.value.map(call => call.type.type).includes(type[0])));
+
+function addCall(value) {
+    const call = {
+        type: types[value],
+        quantity: types[value].expected_results
+    }
+    calls.value.push(call);
+}
 </script>
